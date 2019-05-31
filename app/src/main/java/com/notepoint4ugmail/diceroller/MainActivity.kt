a8e665de6a6ca5f79aabd924894c7c5bbc36e0e9
SumanDora <suman@jackfruitsoftwarelab.com>
2019-05-31 10:37:54 +0530

    Final

--- app/src/main/java/com/notepoint4ugmail/diceroller/MainActivity.kt
+++ app/src/main/java/com/notepoint4ugmail/diceroller/MainActivity.kt
@@ -8,6 +8,7 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    // For late initialisation
     lateinit var diceImage:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +20,17 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.text ="Let's Roll"
 
+        // Button click
         rollButton.setOnClickListener {
            rollDice()
         }
     }
 
     private fun rollDice() {
+        //Getting random value between 1 and 6.
         val randomValue = Random.nextInt(6)+1
 
+        // When expression to show dice image for different random value.
         val drawableResource = when(randomValue){
             1-> R.drawable.dice_1
             2-> R.drawable.dice_2
@@ -36,6 +40,7 @@ class MainActivity : AppCompatActivity() {
             else-> R.drawable.dice_6
         }
 
+        //Setting the dice image.
         diceImage.setImageResource(drawableResource)
     }
 }
