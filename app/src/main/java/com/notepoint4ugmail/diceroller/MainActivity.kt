262f38e1a9577b0b9fc48496b0dbd1edd2beb074
SumanDora <suman@jackfruitsoftwarelab.com>
2019-05-31 10:13:59 +0530

    Initial commit

---
+++ app/src/main/java/com/notepoint4ugmail/diceroller/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.notepoint4ugmail.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton:Button = findViewById(R.id.roll_dice_btn)
+        diceImage = findViewById(R.id.dice_image)
+
+        rollButton.text ="Let's Roll"
+
+        rollButton.setOnClickListener {
+           rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomValue = Random.nextInt(6)+1
+
+        val drawableResource = when(randomValue){
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
